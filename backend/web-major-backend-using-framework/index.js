c4fe6c168693a3d0fb4f6a9baf7a7fa700e339c3
fernandoruanb <fernandosingaporefuture@gmail.com>
2025-09-17 16:01:12 -0300

    ok

--- backend/web-major-backend-using-framework/index.js
+++ backend/web-major-backend-using-framework/index.js
@@ -5,7 +5,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 // Set routes path
 
-const fastify = Fastify({ logger: true });
+const fastify = Fastify({ logger: false });
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
